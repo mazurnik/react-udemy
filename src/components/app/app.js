e56ee424f093ef0a126548c8e962ed742e2f1f67
Mikalai <mazurnikv@gmail.com>
2020-08-03 21:59:00 +0300

    add state component

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -9,6 +9,12 @@ import PostAddForm from "../post-add-form";
 import "./app.css";
 
 const App = () => {
+  const data = [
+    { label: "Going to learn React", important: true, id: 111 },
+    { label: "That is so good", important: false, id: 333 },
+    { label: "I need a break...", important: false, id: 555 },
+  ];
+
   return (
     <div className="app">
       <AppHeader />
@@ -16,7 +22,7 @@ const App = () => {
         <SearchPanel />
         <PostStatusFilter />
       </div>
-      <PostList />
+      <PostList posts={data} />
       <PostAddForm />
     </div>
   );
--- src/components/post-list/post-list.js
+++ src/components/post-list/post-list.js
@@ -5,13 +5,17 @@ import "./post-list.css";
 
 import PostListItem from "../post-list-item";
 
-const PostList = () => {
-  return (
-    <ul className="app-list list-group">
-      <PostListItem />
-      <PostListItem />
-      <PostListItem />
-    </ul>
-  );
+const PostList = ({ posts }) => {
+  const elements = posts.map((item) => {
+    const { id, ...itemProps } = item;
+
+    return (
+      <li key={id} className="list-group-item">
+        <PostListItem l {...itemProps} />
+      </li>
+    );
+  });
+
+  return <ul className="app-list list-group">{elements}</ul>;
 };
 export default PostList;
